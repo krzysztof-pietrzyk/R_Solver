73142fb21061d68380695ab2d208302f3c163144
Krzysztof Pietrzyk <rurek19@gmail.com>
2022-09-27 13:21:26 +0200

    moved AlgorithmStatus to a separate file

---
+++ src/algorithms/AlgorithmStatus.hpp
@@ -0,0 +1,14 @@
+#ifndef AlgorithmStatus_hpp
+#define AlgorithmStatus_hpp
+
+enum AlgorithmStatus
+{
+    UNHANDLED_STATUS,  // For testing and debugging purposes
+    NO_STATUS,         // For algorithms which only analyze the board
+    SUCCESS,           // At least one click performed
+    NO_MOVES,          // No clicks were performed
+    GAME_WON,          // Map is successfully cleared after this algorithm run and game is won
+    GAME_LOST          // This algorithm run resulted in clicking on a mine and game is lost
+};
+
+#endif
